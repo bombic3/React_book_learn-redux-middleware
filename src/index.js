7e38e48166d9d22470724f8a4ea8abe9107ced4a
bombi <bombic3@naver.com>
2022-07-28 16:44:02 +0900

    TEST : loggerMiddleware.js 생성하여 콘솔에 작업순서 나타내기

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom";
+import { applyMiddleware, legacy_createStore } from "redux";
+import { Provider } from "react-redux";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import rootReducer from "./modules";
+import loggerMiddleware from "./lib/loggerMiddleware";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const store = legacy_createStore(rootReducer, applyMiddleware(loggerMiddleware));
+
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
