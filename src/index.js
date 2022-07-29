b58f575b6ffffd62a2245ea633d4fe72baf793f0
bombi <bombic3@naver.com>
2022-07-29 11:55:51 +0900

    REFACTOR : redux-saga를 설치, 사용하여 비동기 작업하기 - 제너레이터 함수

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import { applyMiddleware, legacy_createStore } from "redux";
 import { Provider } from "react-redux";
 import './index.css';
 import App from './App';
-import rootReducer from "./modules";
+import rootReducer, { rootSaga } from "./modules";
 // import loggerMiddleware from "./lib/loggerMiddleware";
 import { createLogger } from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 const logger = createLogger();
-const store = legacy_createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
+const sagaMiddleware = createSagaMiddleware();
+const store = legacy_createStore(
+  rootReducer,
+  applyMiddleware(logger, ReduxThunk, sagaMiddleware)
+);
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
